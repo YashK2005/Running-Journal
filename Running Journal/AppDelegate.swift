262ad44ba393babed3e1c6d60f1a2657ec3bf028
YashK2005 <yashkoth7@gmail.com>
2024-02-19 14:10:39 -0500

    Minor bug fix

--- Running Journal/AppDelegate.swift
+++ Running Journal/AppDelegate.swift
@@ -162,7 +162,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                print("Error: \(nserror), \(nserror.userInfo)")
+                //fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
